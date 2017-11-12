8c086a46340dfceede690a1664c4fcd1d9f2a8cf
keenwon <semanwmj@gmail.com>
2017-11-13 00:45:47 +0800

    不删除 entities
    entities 可能被多个地方引用，目前的方案所限，暂时不删除

--- src/store/v2/fetch/fetch.ts
+++ src/store/v2/fetch/fetch.ts
@@ -70,9 +70,9 @@ export function fetch(config: IFetchRequest, init?: IFetchInit): Promise<any> {
     case 'POST':
       mutationType = 'ENTITIES_UPDATE';
       break;
-    case 'DELETE':
-      mutationType = 'ENTITIES_DELETE';
-      break;
+    // case 'DELETE':
+    //   mutationType = 'ENTITIES_DELETE';
+    //   break;
     case 'PUT':
     case 'PATCH':
     default:
@@ -91,7 +91,7 @@ export function fetch(config: IFetchRequest, init?: IFetchInit): Promise<any> {
       if (schema) {
         let { entities, result } = normalize(responseData, schema);
 
-        _store.commit(mutationType, entities);
+        mutationType && _store.commit(mutationType, entities);
         data = result;
       } else {
         data = responseData;
